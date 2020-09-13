94d999e9c09bdc906b5ec6b2902ec0f8ae2499b5
theaterixcom <elio@theaterix.com>
2020-09-13 19:12:17 -0300

    Adding infinite scrolling

---
+++ scroll/README.md
@@ -0,0 +1,6 @@
+# Vue infinite scroll with JSON API
+A very simple example of a web application that uses the Vue.js framework to get results from an external API.
+
+This application points to an API endpoint that returns a list of JSON results. When the user reaches the page limit, we load more results from the API.
+
+**Note:** *I'm importing libraries in my HTML just to simplify the integration. Maybe you should use dependency managers such as NPM to handle the packages and other resources.*
---
+++ scroll/script.js
@@ -0,0 +1,37 @@
+// Defines the API endpoint.
+const api = 'posts.json';
+
+new Vue({
+    el: "#app",
+    data: {
+        posts: [],
+        limit: 10,
+        busy: false
+    },
+    methods: {
+        loadMore() {
+
+            // Sets the application state to "busy".
+            this.busy = true;
+
+            // Sends a get request to the API endpoint.
+            axios.get(api).then(response => {
+                const append = response.data.slice(
+                    this.posts.length,
+                    this.posts.length + this.limit
+                );
+                this.posts = this.posts.concat(append);
+                this.busy = false;
+            });
+        }
+    },
+
+    created() {
+        // Load the items when the instance was created.
+        this.loadMore();
+    }
+
+});
+
+// Animate on scroll library initialization
+AOS.init();
